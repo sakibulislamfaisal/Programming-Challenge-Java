f5dc793dd0607d2551a165f877262ce1fe1355e1
Foyshal Rahman <57571801+sakibulislamfaisal@users.noreply.github.com>
2024-12-23 21:02:10 +0600

    Bitwise AND Program

---
+++ src/BitwiseAND.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+ class BitwiseOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitwise AND Operator");
+        System.out.print("Enter input your first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter input your second number: ");
+        int num2 = sc.nextInt();
+
+        int result = num1 & num2;
+        System.out.println("The result is: " + result);
+    }
+}
+
+
